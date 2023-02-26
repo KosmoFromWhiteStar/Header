5e79705516aea37fcb94dd6221f3abb2d5e6c8f1
Kosmo <The_Kosmo@outlook.com>
2023-02-26 12:49:57 +0500

    Добавьте файлы проекта.

---
+++ incl/Header.project.h
@@ -0,0 +1,48 @@
+﻿// Header.project.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+#pragma once
+#define ENDL std::endl
+#define POINT(x) x[0]  << '.' << x[1]
+#include <iostream>
+
+bool compare(std::string inP_word, std::string command)
+{
+	for (int i = 0; i < inP_word.length() && i < command.length(); i++)
+	{
+		if (inP_word[i] != command[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int coordinate()
+{
+	int x;
+	std::cout << "Input coordinate: " << std::endl;
+	std::cin >> x;
+	return x;
+}
+
+void scalpel(int x[2], int y[2])
+{
+	std::cout << "Cut from " << POINT(x) << " to " << POINT(y) << ENDL;
+}
+
+void hemostat(int pos[2])
+{
+	std::cout << "Clamped " << POINT(pos) << ENDL;
+}
+
+void tweezers(int pos[2])
+{
+	std::cout << "Tweezers: " << POINT(pos) << ENDL;
+}
+
+void suture(int x[2], int y[2])
+{
+	std::cout << "Sew from " << POINT(x) << " to " << POINT(y) << ENDL;
+}
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
---
+++ src/Header.project.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "Header.project.h"
+
+int main()
+{
+	std::cout << "Surge" << ENDL;
+	std::cout << "Input command" << ENDL;
+
+	do
+	{
+		std::string command = "";
+		std::cin >> command;
+		int x = coordinate(), y = coordinate();
+		
+		bool start_serg = false; // status surgery
+		if (compare(command, "scalpel"))
+		{
+			scalpel(&(x), &(y));
+			start_serg = true;
+		}
+		if (compare(command, "hemostat") && start_serg) scalpel(&(x), &(y));
+		if (compare(command, "tweezers") && start_serg) scalpel(&(x), &(y));
+		if (compare(command, "suture") && start_serg) scalpel(&(x), &(y));
+		if (compare(command, "suture") && start_serg)
+		{
+			start_serg = false;
+			break;
+		}
+
+	} while (true);
+	
+	return 0;
+}
